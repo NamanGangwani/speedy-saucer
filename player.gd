b3f5b35faff60e86428ae5ff7875b602a257e79b
NamanGangwani <NamanGangwani@outlook.com>
2026-05-14 23:31:00 -0500

    added apply_force to saucer

--- player.gd
+++ player.gd
@@ -2,4 +2,11 @@ extends RigidBody2D
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_impulse(Vector2(25, 0))
+	#apply_impulse(Vector2(25, 0))
+	pass
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	apply_force(Vector2(25, 0))
